7226a8c11ae8c1f14eb0cfd56cba555d95e2e442
区汇君 <953386166@qq.com>
2016-12-01 11:16:12 +0800

    Update DBHelper.java
    modify DBHelper.java

--- app/src/main/java/com/ppjun/greendaotest/DBHelper.java
+++ app/src/main/java/com/ppjun/greendaotest/DBHelper.java
@@ -17,51 +17,15 @@ import java.util.TreeMap;
  */
 public class DBHelper extends DaoMaster.OpenHelper {
 
-    private static final SortedMap<Integer, Migration> ALL_MIGRATIONS = new TreeMap<>();
-
-    {
-
-        ALL_MIGRATIONS.put(1, new V1Migration());
-    }
-
-    public DBHelper(Context context) {
-        super(context, GreenDaoUtils.DB_NAME, null);
+  public DBHelper(Context context) {
+        super(context, DB_NAME,null);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase db) {
-        super.onCreate(db);
-        executeMigrations(db, ALL_MIGRATIONS.keySet());
-    }
-
-    private void executeMigrations(SQLiteDatabase db, Set<Integer> integers) {
-        for (Integer version : integers) {
-            ALL_MIGRATIONS.get(version).migrate(db);
-        }
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(Database db, int oldVersion, int newVersion) {
         super.onUpgrade(db, oldVersion, newVersion);
-        SortedMap<Integer, Migration> migrations = ALL_MIGRATIONS.subMap(oldVersion, newVersion);
-        executeMigrations(db, migrations.keySet());
-
-    }
-
-
-    public interface Migration {
-        void migrate(SQLiteDatabase db);
-
-
-    }
-
-    //新增列age，如果有多个操作写多个V1Migration
-    public class V1Migration implements Migration {
-
-        @Override
-        public void migrate(SQLiteDatabase db) {
-            db.execSQL("ALTER TABLE USER ADD COLUMN age");
-        }
+        NoteDao.createTable(db,true);
+        db.execSQL("ALTER TABLE NOTE ADD COLUMN age");
     }
 
 }
